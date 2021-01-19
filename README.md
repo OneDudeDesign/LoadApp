9011526e7196c509c322c9041f720ba73c4317ea
Christopher Lucier <OneDudeDesign@gmail.com>
2021-01-19 11:29:05 -0800

    Step02 basic radiogroup and buttons added, functionality regarding no selection made and a selection is there

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LoadApp
 Android-Kotlin Nanodegree project 3
+
+Attributions:
+Documentation reviewed from:
+https://developer.android.com/reference/android/app/DownloadManager.Request?authuser=1#setDestinationUri(android.net.Uri)
--- app/src/main/java/com/onedudedesign/MainActivity.kt
+++ app/src/main/java/com/onedudedesign/MainActivity.kt
@@ -10,6 +10,10 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,6 +24,8 @@ import timber.log.Timber
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
+    private var radioSelected: Boolean = false
+    private var radioURL = ""
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
@@ -32,10 +38,18 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-
+//TODO: Make it download the correct selection
         custom_button.setOnClickListener {
-            download()
+            setUriFromRadioSelection()
+
+            if (radioSelected) {
+                download()
+                radioSelected = false
+                clearRadioGroupSelection()
+            }
         }
+
+
     }
 
     private val receiver = object : BroadcastReceiver() {
@@ -45,25 +59,58 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    private fun clearRadioGroupSelection (){
+        rg_main.clearCheck()
+    }
+
+    private fun setUriFromRadioSelection() {
+        var id: Int = rg_main.checkedRadioButtonId
+        if (id != -1) {
+            // get the radio button if its checked
+            val rb: RadioButton = findViewById(id)
+            // set the radioSelected flag
+            radioSelected = true
+            Toast.makeText(
+                applicationContext,
+                "When the button was clicked, the radioButton was: ${rb.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            //nothing was selected
+            Toast.makeText(
+                applicationContext,
+                "Nothing was selected, please make a selection",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     private fun download() {
         val request =
-                DownloadManager.Request(Uri.parse(URL))
-                        .setTitle(getString(R.string.app_name))
-                        .setDescription(getString(R.string.app_description))
-                        .setRequiresCharging(false)
-                        .setAllowedOverMetered(true)
-                        .setAllowedOverRoaming(true)
-                        //show a notification on completion
-                        .setNotificationVisibility(VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION)
+            DownloadManager.Request(Uri.parse(URLGitHub))
+                .setTitle(getString(R.string.app_name))
+                .setDescription(getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+                //show a notification on completion
+                .setNotificationVisibility(VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
-                downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        rg_main.clearCheck()
     }
 
     companion object {
-        private const val URL =
-                "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URLGitHub =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URLGlide =
+            "https://github.com/bumptech/glide"
+        private const val URLRetrofit =
+            "https://github.com/square/retrofit"
         private const val CHANNEL_ID = "channelId"
     }
 
