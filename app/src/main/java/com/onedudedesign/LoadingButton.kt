4b701c7cc1ae909bed8111445e5ec5386549fba5
Christopher Lucier <OneDudeDesign@gmail.com>
2021-01-25 09:29:28 -0800

    Step08 Got the loadingbutton and text to draw

--- app/src/main/java/com/onedudedesign/LoadingButton.kt
+++ app/src/main/java/com/onedudedesign/LoadingButton.kt
@@ -3,8 +3,12 @@ package com.onedudedesign
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import timber.log.Timber
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -14,6 +18,19 @@ class LoadingButton @JvmOverloads constructor(
     private var heightSize = 0
 
     private val valueAnimator = ValueAnimator()
+    //paint for the text
+    private val paintText = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.WHITE
+        textAlign = Paint.Align.CENTER
+        textSize = 40.0f
+        typeface = Typeface.create("", Typeface.BOLD)
+    }
+    private val paintButtonStart = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.BLUE
+
+    }
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
@@ -24,9 +41,16 @@ class LoadingButton @JvmOverloads constructor(
 
     }
 
+    override fun performClick(): Boolean {
+        buttonState = ButtonState.Clicked
+        Timber.i("Button State: %s", buttonState.toString())
+        return super.performClick()
+    }
+
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        drawButton(canvas)
 
     }
 
@@ -41,6 +65,24 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
+    }
+
+    private fun drawButton(canvas: Canvas?) {
+       // paint.color = Color.CYAN
+        canvas?.drawRect(
+            0.0f,
+            0.0f,
+            width.toFloat(),
+            height.toFloat(),
+            paintButtonStart
+        )
+        //paint.color = Color.WHITE
+        //paint.textAlign = Paint.Align.CENTER
+        canvas?.drawText(
+            context.getString(R.string.button_name),
+            width / 2.0f, height / 2.0f,
+            paintText
+        )
     }
 
 }
